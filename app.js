7d7c7c89e371284ea64fbae9be7deef1dd101351
wangershi <wangwenkai5052@outlook.com>
2019-09-18 02:01:07 +0800

    cross domain

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const queryString = require('querystring');
 const handleServerRouter = require('./src/router/serverData');
 
 const serverHandle = (req,res) =>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.setHeader('Access-Control-Allow-Methods','PUT,POST,GET,PATCH,DELETE,OPTIONS');
+    res.setHeader('Content-Type','application/json;charset=utf-8');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     req.query = queryString.parse(req.url.split('?')[1]);
     const result = handleServerRouter(req,res);
     if(result){
--- src/router/serverData.js
+++ src/router/serverData.js
@@ -1,11 +1,17 @@
-const handleServerData = (req,res) => {
-    const method = req.method;
-    const url = req.url;
-    const path = url.split('?')[0];
-    if(method === "GET" && path === '/api/test'){
-        let array = [10,20,30,40];
-        return array;
+const handleServerData = (req, res) => {
+  const method = req.method;
+  const url = req.url;
+  const path = url.split('?')[0];
+  if (method === "GET" && path === '/api/test') {
+    let response = {
+      data: {
+        title: '互联网公司排名',
+        xAxisData: ["百度", "阿里巴巴", "腾讯", "新浪"],
+        seriesData: [200000, 300000, 400000, 200000]
+      }
     }
+    return response;
+  }
 }
 
 module.exports = handleServerData;
